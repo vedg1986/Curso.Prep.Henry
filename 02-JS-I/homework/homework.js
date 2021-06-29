4e21ed68cbe5520f721c9a965fd548221eec1d07
vedg1986 <75819701+vedg1986@users.noreply.github.com>
2021-06-29 14:33:13 -0300

    Update homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,8 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "meses del año";
+var nombre="hola mundo"
+const nuevaString ="saludo";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 9;
