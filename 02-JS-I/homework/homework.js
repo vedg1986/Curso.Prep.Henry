182e579b796bf9f4ce544f70628b2b0b0a855874
vedg1986 <75819701+vedg1986@users.noreply.github.com>
2021-06-29 15:33:25 -0300

    Update homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -59,11 +59,12 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (x = y);
+  if (x = y); {
   console.log('true');
 } 
 else if (x != y) {
   console.log('false');
+}
 }
 
 function tienenMismaLongitud(str1, str2) {
